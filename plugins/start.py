dd6d8ded13f593033775f2c232b05aea24b90e5e
RJMALLU <98645849+RJMALLU@users.noreply.github.com>
2022-05-15 15:37:49 +0400

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,7 +10,7 @@ async def start(client,message):
 	𝙷𝙴𝙻𝙻𝙾 {message.from_user.first_name }
         𝙸 𝙰𝙼 𝙲𝙷 𝙵𝙸𝙻𝙴 𝚁𝙴𝙽𝙰𝙽𝙴𝚁 𝙱𝙾𝚃 𝙿𝙻𝚉 𝚂𝙴𝙽𝙳 𝙰𝙽𝚈 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼
         𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝙰𝙽𝙳 𝙴𝙽𝚃𝙴𝚁 𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴 𝚃𝙾 𝚁𝙴𝙽𝙰𝙼𝙴 𝙸𝚃
-	""",reply_to_message_id = message.id ,  
+	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup([[
           InlineKeyboardButton("𝙲𝙷𝙰𝙽𝙽𝙴𝙻" ,url="https://t.me/publicchannalin"), 
 	  InlineKeyboardButton("𝙶𝚁𝙾𝚄𝙿", url="https://t.me/cine_makotta")
